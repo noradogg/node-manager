e7fef55f37e9d1a11d7914fbb0e3800aaab6ff11
noda <ndktr.n9h93@gmail.com>
2023-09-25 22:50:34 +0900

    Done server/client HelloWorld.

--- server/app/src/hello.go
+++ server/app/src/hello.go
@@ -7,8 +7,8 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Fprintf(w, "Hello, world!")
+    fmt.Println("Endpoint Hit: ")
 }
 
 func handleRequests() {
